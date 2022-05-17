f3d17ae87882850d68c4aaada6dad988cb01a37d
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2022-05-17 15:18:10 +0200

    more user table work

--- db/query/user.sql
+++ db/query/user.sql
@@ -27,4 +27,27 @@ SELECT id,
   image,
   status
 FROM "Users"
-ORDER BY id;
+ORDER BY id
+LIMIT $1 OFFSET $2;
+-- name: UpdateUserImage :exec
+UPDATE "Users"
+SET image = $2
+WHERE id = $1;
+-- name: UpdateUserInfo :exec
+UPDATE "Users"
+SET (name, email, image, status, hashed_pw) = ($2, $3, $4, $5, $6)
+where id = $1
+returning $1, $2, $3, $4, $5;
+
+-- -- name: UpdateUserHashedPW :exec
+  -- UPDATE "Users"
+  -- SET hashed_pw = $2
+  -- WHERE id = $1;
+  -- -- name: UpdateUserEmail :exec
+  -- UPDATE "Users"
+  -- SET email = $2
+  -- WHERE id = $1;
+  -- -- name: UpdateUsername :exec
+  -- UPDATE "Users"
+  -- SET name = $2
+  -- WHERE id = $1;
--- db/sqlc/user.sql.go
+++ db/sqlc/user.sql.go
@@ -98,7 +98,13 @@ SELECT id,
   status
 FROM "Users"
 ORDER BY id
+LIMIT $1 OFFSET $2
 `
+
+type ListUsersParams struct {
+	Limit  int32 `json:"limit"`
+	Offset int32 `json:"offset"`
+}
 
 type ListUsersRow struct {
 	ID     int64          `json:"id"`
@@ -108,8 +114,8 @@ type ListUsersRow struct {
 	Status sql.NullString `json:"status"`
 }
 
-func (q *Queries) ListUsers(ctx context.Context) ([]ListUsersRow, error) {
-	rows, err := q.db.QueryContext(ctx, listUsers)
+func (q *Queries) ListUsers(ctx context.Context, arg ListUsersParams) ([]ListUsersRow, error) {
+	rows, err := q.db.QueryContext(ctx, listUsers, arg.Limit, arg.Offset)
 	if err != nil {
 		return nil, err
 	}
@@ -135,4 +141,56 @@ func (q *Queries) ListUsers(ctx context.Context) ([]ListUsersRow, error) {
 		return nil, err
 	}
 	return items, nil
+}
+
+const updateUserImage = `-- name: UpdateUserImage :exec
+UPDATE "Users"
+SET image = $2
+WHERE id = $1
+`
+
+type UpdateUserImageParams struct {
+	ID    int64          `json:"id"`
+	Image sql.NullString `json:"image"`
+}
+
+func (q *Queries) UpdateUserImage(ctx context.Context, arg UpdateUserImageParams) error {
+	_, err := q.db.ExecContext(ctx, updateUserImage, arg.ID, arg.Image)
+	return err
+}
+
+const updateUserInfo = `-- name: UpdateUserInfo :exec
+UPDATE "Users"
+SET (name, email, image, status, hashed_pw) = ($2, $3, $4, $5, $6)
+where id = $1
+returning $1, $2, $3, $4, $5
+`
+
+type UpdateUserInfoParams struct {
+	ID       int64          `json:"id"`
+	Name     string         `json:"name"`
+	Email    string         `json:"email"`
+	Image    sql.NullString `json:"image"`
+	Status   sql.NullString `json:"status"`
+	HashedPw string         `json:"hashedPw"`
+}
+
+type UpdateUserInfoRow struct {
+	Column1 interface{} `json:"column1"`
+	Column2 interface{} `json:"column2"`
+	Column3 interface{} `json:"column3"`
+	Column4 interface{} `json:"column4"`
+	Column5 interface{} `json:"column5"`
+}
+
+func (q *Queries) UpdateUserInfo(ctx context.Context, arg UpdateUserInfoParams) error {
+	_, err := q.db.ExecContext(ctx, updateUserInfo,
+		arg.ID,
+		arg.Name,
+		arg.Email,
+		arg.Image,
+		arg.Status,
+		arg.HashedPw,
+	)
+	return err
 }
