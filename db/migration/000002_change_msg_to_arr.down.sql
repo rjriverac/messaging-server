72da449c98a437530733fc108794a144d5a69bf6
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2022-05-18 12:26:24 +0200

    reverted planned migration

--- db/migration/000002_change_msg_to_arr.down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "Conversation"
-ALTER COLUMN "Messages" DROP DEFAULT,
-  ALTER COLUMN "Messages" type bigint USING "Messages"[1]::bigint;
--- db/migration/000002_change_msg_to_arr.up.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "Conversation"
-  ALTER COLUMN "Messages" type bigint[] USING array["Messages"]::BIGINT[],
-  ALTER COLUMN SET DEFAULT "{}";
