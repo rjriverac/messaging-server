22572ecf4598355e1a4a84292c5b4647adbc4dc2
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2022-05-25 13:23:01 +0200

    get user route complete

--- api/account.go
+++ api/account.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -32,4 +33,27 @@ func (server *Server) createUser(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 	}
 	ctx.JSON(http.StatusOK, user)
+}
+
+type GetUserRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getUser(ctx *gin.Context) {
+	var req GetUserRequest
+	if err:= ctx.ShouldBindUri(&req); err!=nil{
+		ctx.JSON(http.StatusBadRequest,errorResponse(err))
+		return
+	}
+
+	user,err := server.store.GetUser(ctx,req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound,errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError,errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK,user)
 }
